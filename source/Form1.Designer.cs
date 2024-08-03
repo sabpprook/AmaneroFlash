0ddd7daa729790430760342df0ed9d0de5b3cc6c
YU-YEN HSU <sabpprook@gmail.com>
2024-08-03 10:34:20 +0800

    update

--- source/Form1.Designer.cs
+++ source/Form1.Designer.cs
@@ -34,9 +34,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btn_FlashCPLD = new System.Windows.Forms.Button();
             this.btn_FlashCPU = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox_ComPorts = new System.Windows.Forms.ComboBox();
-            this.btn_RefreshCom = new System.Windows.Forms.Button();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.panel = new System.Windows.Forms.Panel();
             this.linkLabel = new System.Windows.Forms.LinkLabel();
@@ -45,7 +42,7 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(17, 64);
+            this.label1.Location = new System.Drawing.Point(17, 17);
             this.label1.Margin = new System.Windows.Forms.Padding(8, 8, 5, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 24);
@@ -57,7 +54,10 @@
             // 
             this.comboBox_CPLD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_CPLD.FormattingEnabled = true;
-            this.comboBox_CPLD.Location = new System.Drawing.Point(145, 63);
+            this.comboBox_CPLD.IntegralHeight = false;
+            this.comboBox_CPLD.Location = new System.Drawing.Point(145, 16);
+            this.comboBox_CPLD.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.comboBox_CPLD.MaxDropDownItems = 10;
             this.comboBox_CPLD.Name = "comboBox_CPLD";
             this.comboBox_CPLD.Size = new System.Drawing.Size(300, 28);
             this.comboBox_CPLD.TabIndex = 1;
@@ -66,14 +66,16 @@
             // 
             this.comboBox_CPU.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_CPU.FormattingEnabled = true;
-            this.comboBox_CPU.Location = new System.Drawing.Point(145, 110);
+            this.comboBox_CPU.IntegralHeight = false;
+            this.comboBox_CPU.Location = new System.Drawing.Point(145, 67);
+            this.comboBox_CPU.MaxDropDownItems = 10;
             this.comboBox_CPU.Name = "comboBox_CPU";
             this.comboBox_CPU.Size = new System.Drawing.Size(300, 28);
             this.comboBox_CPU.TabIndex = 3;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(17, 111);
+            this.label2.Location = new System.Drawing.Point(17, 68);
             this.label2.Margin = new System.Windows.Forms.Padding(8, 8, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 24);
@@ -83,60 +85,32 @@
             // 
             // btn_FlashCPLD
             // 
-            this.btn_FlashCPLD.Location = new System.Drawing.Point(465, 63);
+            this.btn_FlashCPLD.Location = new System.Drawing.Point(465, 16);
             this.btn_FlashCPLD.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
             this.btn_FlashCPLD.Name = "btn_FlashCPLD";
             this.btn_FlashCPLD.Size = new System.Drawing.Size(100, 28);
             this.btn_FlashCPLD.TabIndex = 4;
+            this.btn_FlashCPLD.Tag = "CPLD";
             this.btn_FlashCPLD.Text = "Flash";
             this.btn_FlashCPLD.UseVisualStyleBackColor = true;
-            this.btn_FlashCPLD.Click += new System.EventHandler(this.btn_FlashCPLD_Click);
+            this.btn_FlashCPLD.Click += new System.EventHandler(this.btn_Flash_Click);
             // 
             // btn_FlashCPU
             // 
-            this.btn_FlashCPU.Location = new System.Drawing.Point(465, 110);
+            this.btn_FlashCPU.Location = new System.Drawing.Point(465, 67);
             this.btn_FlashCPU.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
             this.btn_FlashCPU.Name = "btn_FlashCPU";
             this.btn_FlashCPU.Size = new System.Drawing.Size(100, 28);
             this.btn_FlashCPU.TabIndex = 5;
+            this.btn_FlashCPU.Tag = "CPU";
             this.btn_FlashCPU.Text = "Flash";
             this.btn_FlashCPU.UseVisualStyleBackColor = true;
-            this.btn_FlashCPU.Click += new System.EventHandler(this.btn_FlashCPU_Click);
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(17, 17);
-            this.label3.Margin = new System.Windows.Forms.Padding(8, 8, 5, 15);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 24);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "COM Port:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // comboBox_ComPorts
-            // 
-            this.comboBox_ComPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_ComPorts.FormattingEnabled = true;
-            this.comboBox_ComPorts.Location = new System.Drawing.Point(145, 16);
-            this.comboBox_ComPorts.Name = "comboBox_ComPorts";
-            this.comboBox_ComPorts.Size = new System.Drawing.Size(150, 28);
-            this.comboBox_ComPorts.TabIndex = 7;
-            // 
-            // btn_RefreshCom
-            // 
-            this.btn_RefreshCom.Location = new System.Drawing.Point(325, 16);
-            this.btn_RefreshCom.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
-            this.btn_RefreshCom.Name = "btn_RefreshCom";
-            this.btn_RefreshCom.Size = new System.Drawing.Size(120, 28);
-            this.btn_RefreshCom.TabIndex = 8;
-            this.btn_RefreshCom.Text = "Refresh";
-            this.btn_RefreshCom.UseVisualStyleBackColor = true;
-            this.btn_RefreshCom.Click += new System.EventHandler(this.btn_RefreshCom_Click);
+            this.btn_FlashCPU.Click += new System.EventHandler(this.btn_Flash_Click);
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 169);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 20, 3, 5);
+            this.progressBar.Location = new System.Drawing.Point(12, 139);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 10, 3, 5);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(560, 28);
             this.progressBar.TabIndex = 9;
@@ -144,12 +118,9 @@
             // panel
             // 
             this.panel.Controls.Add(this.linkLabel);
-            this.panel.Controls.Add(this.label3);
             this.panel.Controls.Add(this.progressBar);
             this.panel.Controls.Add(this.label1);
-            this.panel.Controls.Add(this.btn_RefreshCom);
             this.panel.Controls.Add(this.comboBox_CPLD);
-            this.panel.Controls.Add(this.comboBox_ComPorts);
             this.panel.Controls.Add(this.label2);
             this.panel.Controls.Add(this.comboBox_CPU);
             this.panel.Controls.Add(this.btn_FlashCPU);
@@ -157,7 +128,7 @@
             this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel.Location = new System.Drawing.Point(0, 0);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(584, 211);
+            this.panel.Size = new System.Drawing.Size(584, 181);
             this.panel.TabIndex = 10;
             // 
             // linkLabel
@@ -165,12 +136,12 @@
             this.linkLabel.AutoSize = true;
             this.linkLabel.DisabledLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.linkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.linkLabel.Location = new System.Drawing.Point(462, 19);
+            this.linkLabel.Location = new System.Drawing.Point(418, 109);
             this.linkLabel.Name = "linkLabel";
-            this.linkLabel.Size = new System.Drawing.Size(103, 20);
+            this.linkLabel.Size = new System.Drawing.Size(154, 20);
             this.linkLabel.TabIndex = 10;
             this.linkLabel.TabStop = true;
-            this.linkLabel.Text = "AmaneroFlash";
+            this.linkLabel.Text = "Github: AmaneroFlash";
             this.linkLabel.VisitedLinkColor = System.Drawing.Color.Red;
             this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
             // 
@@ -178,7 +149,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 211);
+            this.ClientSize = new System.Drawing.Size(584, 181);
             this.Controls.Add(this.panel);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -202,9 +173,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_FlashCPLD;
         private System.Windows.Forms.Button btn_FlashCPU;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox_ComPorts;
-        private System.Windows.Forms.Button btn_RefreshCom;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Panel panel;
         private System.Windows.Forms.LinkLabel linkLabel;
--- source/Form1.cs
+++ source/Form1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Ports;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,7 +19,6 @@ namespace AmaneroFlash
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            btn_RefreshCom.PerformClick();
             ListFirmware();
         }
 
@@ -39,63 +39,90 @@ namespace AmaneroFlash
             }
         }
 
+        private string GetComPort()
+        {
+            var port = string.Empty;
+            var ports = new List<string>();
+
+            var reg = Registry.LocalMachine.OpenSubKey("HARDWARE\\DEVICEMAP\\SERIALCOMM");
+            foreach (var val in reg.GetValueNames())
+            {
+                ports.Add(reg.GetValue(val) as string);
+            }
+
+            reg = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\USB");
+            foreach (var sub in reg.GetSubKeyNames())
+            {
+                if (sub.Contains("PID_6124"))
+                {
+                    reg = reg.OpenSubKey(sub);
+                    break;
+                }
+            }
+            foreach (var sub in reg.GetSubKeyNames())
+            {
+                var device = reg.OpenSubKey(sub).OpenSubKey("Device Parameters");
+                var portname = device.GetValue("PortName") as string;
+                if (ports.Contains(portname))
+                {
+                    port = portname;
+                    break;
+                }
+            }
+            return port;
+        }
+
         private void FlashFW(string comport, FWInfo fw)
         {
             Invoke(new Action(() =>
             {
                 progressBar.Value = 0;
-                progressBar.Maximum = fw.GetSize();
+                progressBar.Maximum = fw.Size;
             }));
 
-            var sp = new SerialPort(comport, 115200, Parity.None, 8, StopBits.One);
-
-            sp.Open();
-            foreach (var line in fw.GetData())
+            try
             {
-                sp.Write(line);
-                Thread.Sleep(10);
-                if (sp.IsOpen)
-                    sp.ReadExisting();
+                var sp = new SerialPort(comport, 115200, Parity.None, 8, StopBits.One);
 
-                var length = line.Length;
-                Invoke(new Action(() => progressBar.Value += length));
-                Console.WriteLine($"send: {length} bytes");
+                sp.Open();
+                foreach (var line in fw.Data)
+                {
+                    sp.Write(line);
+                    Thread.Sleep(20);
+
+                    if (sp.IsOpen)
+                        sp.ReadExisting();
+
+                    var length = line.Length;
+                    Invoke(new Action(() => progressBar.Value += length));
+                }
+                sp.Close();
+
+                Thread.Sleep(500);
             }
-            sp.Close();
+            catch
+            {
+            }
 
             Invoke(new Action(() => progressBar.Value = progressBar.Maximum));
         }
 
-        private void btn_RefreshCom_Click(object sender, EventArgs e)
-        {
-            comboBox_ComPorts.Items.Clear();
-            var ports = SerialPort.GetPortNames();
-            foreach (var port in ports)
-            {
-                if (!comboBox_ComPorts.Items.Contains(port))
-                    comboBox_ComPorts.Items.Add(port);
-            }
-            if (comboBox_ComPorts.Items.Count > 0)
-            {
-                comboBox_ComPorts.SelectedIndex = 0;
-            }
-        }
-
-        private async void btn_FlashCPLD_Click(object sender, EventArgs e)
+        private async void btn_Flash_Click(object sender, EventArgs e)
         {
             panel.Enabled = false;
-            var port = comboBox_ComPorts.Text;
-            var fw = comboBox_CPLD.SelectedItem as FWInfo;
-            await Task.Run(() => FlashFW(port, fw));
-            panel.Enabled = true;
-        }
 
-        private async void btn_FlashCPU_Click(object sender, EventArgs e)
-        {
-            panel.Enabled = false;
-            var port = comboBox_ComPorts.Text;
-            var fw = comboBox_CPU.SelectedItem as FWInfo;
-            await Task.Run(() => FlashFW(port, fw));
+            var comport = GetComPort();
+
+            FWInfo fw = null;
+            var option = (sender as Button).Tag.ToString();
+            if (option == "CPLD") fw = comboBox_CPLD.SelectedItem as FWInfo;
+            if (option == "CPU") fw = comboBox_CPU.SelectedItem as FWInfo;
+
+            if (!string.IsNullOrEmpty(comport) && fw != null)
+            {
+                await Task.Run(() => FlashFW(comport, fw));
+            }
+
             panel.Enabled = true;
         }
 
@@ -109,6 +136,10 @@ namespace AmaneroFlash
     {
         private FileInfo info;
 
+        public string[] Data { get { return File.ReadAllLines(info.FullName); } }
+
+        public int Size { get { return (int)info.Length; } }
+
         public FWInfo(string path)
         {
             info = new FileInfo(path);
@@ -117,16 +148,6 @@ namespace AmaneroFlash
         public override string ToString()
         {
             return Path.GetFileNameWithoutExtension(info.FullName);
-        }
-
-        public string[] GetData()
-        {
-            return File.ReadAllLines(info.FullName);
-        }
-
-        public int GetSize()
-        {
-            return (int)info.Length;
         }
     }
 }
